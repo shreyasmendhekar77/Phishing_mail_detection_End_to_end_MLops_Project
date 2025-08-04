2d4d52980adb5152ddd8242bc3607af41153192f
ShreyasDominator <mt23mcs004@students.vnit.ac.in>
2025-08-05 00:28:58 +0530

    Done with training data research

--- src/Phising_mail_detection/config/configuration.py
+++ src/Phising_mail_detection/config/configuration.py
@@ -13,3 +13,10 @@ class Data_preprocessing_config:
     root_dir: Path
     unzip_data_dir: Path
     vector_embed_model: Path
+
+
+@dataclass(frozen=True)
+class ModelTrainingConfig:
+    root_dir :Path
+    training_data_path: Path
+    trained_model_dir: Path
